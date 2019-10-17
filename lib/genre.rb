a13fafce68b531040bd13ddcab21eafab2b7203c
generous-vector-9581 <ojappah@gmail.com>
2019-10-17 01:17:31 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,21 +1,27 @@
-	class Genre
+  
+class Genre
+  attr_accessor :name
 
-  attr_reader :name, :genre
-  attr_accessor :song
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    Song.all { |song| song.genre == self }
   end
 
   def artists
-    Song.all.map do |song|
-      song.artist
-    end
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
   end
 end
