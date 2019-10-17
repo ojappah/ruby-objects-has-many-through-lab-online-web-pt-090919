c6e4335aac25bfc5391d0a19960eedbc5f193ac4
generous-vector-9581 <ojappah@gmail.com>
2019-10-17 01:18:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,24 +1,26 @@
 class Patient
+  attr_accessor :name
 
-  attr_reader :name, :appointment, :doctor
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
+    Appointment.all.select { |appointment| appointment.patient == self }
   end
 
   def doctors
-    Appointment.all.map do |appointment|
-      appointment.doctor
-    end
+    appointments.map(&:doctor)
   end
 end
